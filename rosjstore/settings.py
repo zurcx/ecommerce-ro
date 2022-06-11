44c1eca266007e7b1d28d4ecdea27f77274275e6
zurcx <luizfabiodacruz@gmail.com>
2022-06-10 22:07:59 -0300

    setup user

--- rosjstore/settings.py
+++ rosjstore/settings.py
@@ -31,12 +31,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
+    # Djando apps
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # My apps
+
+    'users.apps.UsersConfig'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +129,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'users.User'
---
+++ users/admin.py
@@ -0,0 +1,13 @@
+from atexit import register
+
+from django.contrib import admin
+from django.contrib.auth import admin as auth_admin
+
+from .forms import UserChangeForm, UserCreateForm
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(auth_admin.UserAdmin):
+    form = UserChangeForm
+    add_form = UserCreateForm
