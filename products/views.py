becf05aa451939c9b5ede56d42d7ca7b9c5388f7
zurcx <luizfabiodacruz@gmail.com>
2022-06-11 23:24:38 -0300

    products correct

--- products/views.py
+++ products/views.py
@@ -1,5 +1,3 @@
-
-
 #from cart.forms import CartAddProductForm
 from django.shortcuts import get_object_or_404
 from django.views.generic import DetailView, ListView
@@ -9,7 +7,7 @@ from .models import Category, Product
 
 class ProductDetailView(DetailView):
     queryset = Product.available.all()
-    #extra_context = {"form": CartAddProductForm()}
+    # extra_context = {"form": CartAddProductForm()}
 
 
 class ProductListView(ListView):
